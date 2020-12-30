9ef7e04fcf1e65ddb1cc211f720519fc134797d7
Jasper M-W <jasper@ngita.co.nz>
2020-12-31 11:47:32 +1300

    Started integrating the cubism-es d3 graph

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -51,7 +51,7 @@ pub fn get_data(state: Data<Mutex<AppState>>) -> HttpResponse {
 
     let result = format!(r#"{{ "voltages": {} }}"#, json_voltage);
 
-    return HttpResponse::Ok().body(result)
+    return HttpResponse::Ok().content_type("application/json").body(result)
 }
 
 // Mounts to /api/alive
@@ -67,5 +67,5 @@ pub fn device_info(state: Data<Mutex<AppState>>) -> HttpResponse {
     let device_info = locked_state.device_info.clone();
     drop(locked_state);
     
-    return HttpResponse::Ok().body(device_info.to_string());
+    return HttpResponse::Ok().content_type("application/json").body(device_info.to_string());
 }
--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,38 @@
 import $ from "jquery";
-// import {context} from "cubism-es";
+import * as d3 from "d3";
+import {context} from "cubism-es";
 import {Modal} from 'bootstrap';
+// import es6-shim;
+
+
+let data = {};
+
+let current_voltage_points = []
+let server_alive = true;
+
+// Absolutely needed
+const isEven = (a) => (a % 2 == 0);
+const zeroPad = (num, places) => String(num).padStart(places, '0')
 
 const getData = async () => {
     $.ajax({
         type: "get", url: "/api/data",
         success: function (data, text) {
-            current_voltage_points.push(JSON.parse(data));
+            let voltages = data["voltages"];
+            for (let i = 0; i < voltages.length; i++) {
+                let voltage = voltages[i];
+                current_voltage_points.push(voltage);
+            }
+            console.log(voltages);
+            let ms = voltages[voltages.length -1];
+
+            let seconds = ms / 1000;
+            let hours = parseInt( seconds / 3600 ); // 3,600 seconds in 1 hour
+            seconds = seconds % 3600; // seconds remaining after extracting hours
+            let minutes = parseInt( seconds / 60 ); // 60 seconds in 1 minute
+            seconds = seconds % 60;
+
+            $("#info-time-running").html(zeroPad(hours,2)+":"+zeroPad(minutes,2)+":"+zeroPad(seconds.toFixed(3),3));
 
         },
         error: function (request, status, error) {
@@ -19,14 +45,7 @@ const getData = async () => {
     });
 };
 
-let deviceInfo = {};
-
-let current_voltage_points = []
-let server_alive = true;
-
 let interval = setInterval(getData, 400);
-
-
 
 function checkAlive() {
     let serverStatusModel = $("#serverDisconnectedModal");
@@ -59,26 +78,77 @@ function checkAlive() {
     });
 }
 
+function cubismInitialization() {
+    var ctx = context()
+        .step(1e4)
+        .size(1280);
+
+    d3.select("#voltage-graph-area").selectAll(".axis")
+        .data(["top", "bottom"])
+        .enter().append("div")
+        .attr("class", function(d) { return d + " axis"; })
+        .each(function(d) { ctx.axis().ticks(12).orient(d).render(d3.select(this)); });
+
+    const r = d3.select("#voltage-graph-area").append("div")
+        .attr("class", "rule");
+
+    ctx.rule().render(r);
+
+    const h = d3.select("#voltage-graph-area").selectAll(".horizon")
+        .data(d3.range(1, 10).map(random))
+        .enter().insert("div", ".bottom")
+        .attr("class", "horizon");
+    ctx.horizon()
+        .extent([-10, 10])
+        .render(h);
+
+    ctx.on("focus", function(i) {
+        d3.selectAll(".value").style("right", i == null ? null : ctx.size() - i + "px");
+    });
+
+    // Replace this with context.graphite and graphite.metric!
+    function random(x) {
+        var value = 0,
+            values = [],
+            i = 0,
+            last;
+        return ctx.metric(function(start, stop, step, callback) {
+            start = +start, stop = +stop;
+            if (isNaN(last)) last = start;
+            while (last < stop) {
+                last += step;
+                value = Math.max(-10, Math.min(10, value + .8 * Math.random() - .4 + .2 * Math.cos(i += x * .02)));
+                values.push(value);
+            }
+            callback(null, values = values.slice((start - stop) / step));
+        }, x);
+    }
+}
+
 $(() => {
-    // setInterval(async () => {
-    //     checkAlive();
-    // }, 500);
+    setInterval(async () => {
+        checkAlive();
+    }, 500);
     $.ajax({
         type: "get", url: "/api/device-info",
         success: (data, text) => {
-            deviceInfo = JSON.parse(data);
-            $("#info-picoscope-type").html("PicoScope " + deviceInfo["pico_scope_type"]);
-            $("#info-channel-count").html(deviceInfo["channel_info"].length);
-            $("#info-vertual-channel-count").html(deviceInfo["channel_info"].map((e) => e["virt_channels"]).reduce((a,b) => a + b));
-            $("#info-refresh-rate").html(deviceInfo["refresh_rate"]);
-            $("#info-voltage-range").html(deviceInfo["channel_info"][0]["voltage_range"])
+            let virtualChannelCount = data["channel_info"].map((e) => e["virt_channels"]).reduce((a,b) => a + b)
+            let ChannelCount = data["channel_info"].length
+
+            $("#info-picoscope-type").html("PicoScope " + data["pico_scope_type"]);
+            $("#info-channel-count").html(ChannelCount + " (" + data["channel_info"].map((a) => a["channel"]).join(" | ") + ")");
+            $("#info-virtual-channel-count").html(virtualChannelCount);
+
+            $("#info-refresh-rate").html(data["refresh_rate"]+" / "+(data["refresh_rate"]*ChannelCount)/(virtualChannelCount));
+            $("#info-voltage-range").html(data["channel_info"].map((e) => e["channel"] + ": " + e["voltage_range"]).join(", "))
         },
         error: (request, status, error) => {
-            console.log("Error retrieveing device data.");
+            console.log("Error retrieving device data.");
             console.table({
                 "error": error,
                 "status": status
             })
         }
-    })
+    });
+    cubismInitialization();
 });
