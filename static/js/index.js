37bd7ce2d83a17a33327c8cb4768e184371574c7
Jasper M-W <jasper@ngita.co.nz>
2020-12-30 16:36:36 +1300

    Swaped bootstrap into node_modules, and added a server offline dialog

--- static/js/index.js
+++ static/js/index.js
@@ -1,44 +1,84 @@
 import $ from "jquery";
-import {context} from "cubism-es";
+// import {context} from "cubism-es";
+import {Modal} from 'bootstrap';
 
 const getData = async () => {
-    try {
-        let response = await $.get("api/data");
-        current_voltage_points.push(response);
-    } catch (e) {
-        console.log("Error fetching data from API");
-    }
+    $.ajax({
+        type: "get", url: "/api/data",
+        success: function (data, text) {
+            current_voltage_points.push(JSON.parse(data));
+
+        },
+        error: function (request, status, error) {
+            console.log("Error fetching voltage from API");
+            console.table({
+                "error": error,
+                "status": status
+            });
+        }
+    });
 };
+
+let deviceInfo = {};
 
 let current_voltage_points = []
 let server_alive = true;
 
 let interval = setInterval(getData, 400);
 
-setInterval(async () => {
-    checkAlive();
-}, 500);
+
 
 function checkAlive() {
+    let serverStatusModel = $("#serverDisconnectedModal");
+    
+
     $.ajax({
         type: "get", url: "/api/alive",
-        success: function (data, text) {
+        success: (data, text) => {
             if (!server_alive) {
                 console.log("Server connection regained.")
+                
                 serverStatusModel.hide();
                 server_alive = true;
                 setInterval(getData, 400);
             }
 
         },
-        error: function (request, status, error) {
+        error: (request, status, error) => {
             if (server_alive) {
                 console.log("Server connection lost.")
+                
                 serverStatusModel.show();
                 server_alive = false;
-                clearInterval(interval);
+                clearInterval(getData, 400);
+                
+                let myModalEl = new Modal($("#serverDisconnectedModal"));
+                myModalEl.show();
             }
         }
     });
-    let serverStatusModel = $("#serverDisconnectedModal");
 }
+
+$(() => {
+    setInterval(async () => {
+        checkAlive();
+    }, 500);
+    $.ajax({
+        type: "get", url: "/api/device-info",
+        success: (data, text) => {
+            deviceInfo = JSON.parse(data);
+            $("#info-picoscope-type").html("PicoScope " + deviceInfo["pico_scope_type"]);
+            $("#info-channel-count").html(deviceInfo["channel_info"].length);
+            $("#info-vertual-channel-count").html(deviceInfo["channel_info"].map((e) => e["virt_channels"]).reduce((a,b) => a + b));
+            $("#info-refresh-rate").html(deviceInfo["refresh_rate"]);
+            $("#info-voltage-range").html(deviceInfo["channel_info"][0]["voltage_range"])
+        },
+        error: (request, status, error) => {
+            console.log("Error retrieveing device data.");
+            console.table({
+                "error": error,
+                "status": status
+            })
+        }
+    })
+});
