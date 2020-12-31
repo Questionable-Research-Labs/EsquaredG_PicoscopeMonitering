3bca79b7c18931de379ed91d20f50215ab311390
Jasper M-W <jasper@ngita.co.nz>
2021-01-01 00:03:02 +1300

    New Year Automated commit

---
+++ BuildPushNewYear.sh
@@ -0,0 +1,8 @@
+cd /home/fallstop/Documents/projects/EsquaredG_PicoscopeMonitering
+termdown 2021-01-01
+cargo clean
+cargo build
+figlet Welcome to 2020 Part 2.
+git commit -m "New Year go brrrr"
+git push --force
+bash
