1aa5026c58a8fb742c64d44ff694fcbf55bff90b
Jasper M-W <jasper@qrl.nz>
2021-06-24 15:49:59 +1200

    More work on virt_channel

--- src/virt_channels.rs
+++ src/virt_channels.rs
@@ -10,7 +10,7 @@ pub type VirtChannel = usize;
 pub type VirtSamples = HashMap<VirtChannel, f64>;
 
 pub enum VirtChannelError {
-    NotEnoughData
+    NotEnoughData,
 }
 
 // fn generate_virtal_sample_layout() -> VirtSamples {
@@ -23,77 +23,78 @@ pub enum VirtChannelError {
 
 pub fn split_into_virt_channels(
     raw_data: &HashMap<PicoChannel, (usize, Vec<f64>, String)>,
-    picoscope_sample_speed: u64
-) -> Result<Vec<VirtSamples>,VirtChannelError> {
-
+    picoscope_sample_speed: u64,
+) -> Result<Vec<VirtSamples>, VirtChannelError> {
     let const_config = ConstConfig::get_config();
     let virtual_samples: Vec<VirtSamples> = vec![];
     let mut i = 0;
 
     // Estimate samples per arudino switch
-    let est_sample_width: usize = (picoscope_sample_speed as usize)/const_config.arudino_hz;
+    let est_sample_width: usize = (picoscope_sample_speed as usize) / const_config.arudino_hz;
 
     // Find High points in data (indicateding a sync pulse)
     let sync_pulses: HashMap<PicoChannel, Vec<usize>> = HashMap::new();
     for (channel, data) in raw_data {
-        let sync_pulses = find_sync_pulse(&data.1,est_sample_width);
+        let sync_pulses = find_sync_pulse(&data.1, est_sample_width);
     }
     if sync_pulses.len() < 2 {
         return Err(VirtChannelError::NotEnoughData);
     }
 
-    // Determine points to extract from data
-    let virt_channel_timing: HashMap<PicoChannel, Vec<(VirtChannel,usize)>> = HashMap::new();
+    // Determine points to extract from data, and extracts an averaged result
+    let virt_channel_samples: HashMap<PicoChannel, Vec<(VirtChannel, f64)>> = HashMap::new();
     for (channel, data) in sync_pulses {
-        virt_channel_timing.insert(channel, determine_virt_channel_points(&sync_pulses[&channel], &raw_data[&channel].1,const_config.virt_channel_count));
-
+        virt_channel_samples.insert(
+            channel,
+            determine_virt_channel_samples(
+                &sync_pulses[&channel],
+                const_config.virt_channel_count,
+                &raw_data,
+            ),
+        );
     }
-
-    
     // Flatten Pico channels into Virt Channels
-
-    // Use indexs to generate an averaged sample
-    
 
     return Ok(virtual_samples);
 }
 
-fn find_sync_pulse(input: &Vec<f64>,est_sample_width: usize) -> Vec<usize> {
+/// Takes Picoscope data and determines the center of all the sync pulses
+fn find_sync_pulse(input: &Vec<f64>, est_sample_width: usize) -> Vec<usize> {
     let const_config = ConstConfig::get_config();
 
     let final_sync_points: Vec<usize> = vec![];
 
     // Find all datapoints beyond threshold
     let mut elevated_points: Vec<usize> = vec![];
-    for (index,data_point) in input.iter().enumerate() {
+    for (index, data_point) in input.iter().enumerate() {
         if *data_point > const_config.sync_point_threashold {
             elevated_points.push(index)
         }
     }
 
     // Classifiy points into blocks
-    let mut raw_block_points: Vec<(usize,usize)> = vec![];
-    let mut current_block: (usize,usize) = (0,0); // (start,end)
-    let upper_sample_width = ((est_sample_width as f32)*(1f32+const_config.arduino_hz_tolerence)).round() as usize;
+    let mut raw_block_points: Vec<(usize, usize)> = vec![];
+    let mut current_block: (usize, usize) = (0, 0); // (start,end)
+    let upper_sample_width =
+        ((est_sample_width as f32) * (1f32 + const_config.arduino_hz_tolerence)).round() as usize;
     for (index, point) in elevated_points.iter().enumerate() {
         if index - current_block.0 > est_sample_width {
             raw_block_points.push(current_block);
             current_block.0 = index
-        }
-        else {
+        } else {
             current_block.1 = index
         }
     }
     raw_block_points.push(current_block);
 
-
     // Scan and find midpoints of good blocks
     // Algerithm is jank so it either creates an empty block at start or an empty one, so we remove it
     raw_block_points.remove(0);
-    let lower_sample_width = ((est_sample_width as f32)*(1f32-const_config.arduino_hz_tolerence)).round() as usize;
+    let lower_sample_width =
+        ((est_sample_width as f32) * (1f32 - const_config.arduino_hz_tolerence)).round() as usize;
     for block in raw_block_points.clone() {
-        if block.0-block.1 > lower_sample_width {
-            let mid_point = (block.0-block.1)/2;
+        if block.0 - block.1 > lower_sample_width {
+            let mid_point = (block.0 - block.1) / 2;
             final_sync_points.push(mid_point)
         }
     }
@@ -101,24 +102,50 @@ fn find_sync_pulse(input: &Vec<f64>,est_sample_width: usize) -> Vec<usize> {
     return final_sync_points;
 }
 
-fn determine_virt_channel_points(sync_points: &Vec<usize>, fullData: &Vec<f64>, virt_channel_count: usize) -> Vec<(VirtChannel,usize)> {
-    let mut virt_channel_points: Vec<(VirtChannel,usize)> = vec![];
-    let mut cumulitive_diff: usize = 0;
-    
+/// Calculates the mid-point of all the virtual channels.
+fn determine_virt_channel_samples(
+    sync_points: &Vec<usize>,
+    virt_channel_count: usize,
+    full_data: &Vec<f64>,
+) -> Vec<(VirtChannel, f64)> {
+    let mut virt_channel_samples: Vec<(VirtChannel, f64)> = vec![];
+    let mut cumulative_diff: usize = 0;
+
     // Find spacing for data points in between sync points
-    for (round, pulse_index) in (&sync_points[1..(sync_points.len()-1)]).iter().enumerate() {
-        let diff = sync_points[round-1] - pulse_index;
-        cumulitive_diff += diff;
-        let spacing = diff/virt_channel_count;
+    for (round, pulse_index) in (&sync_points[1..(sync_points.len() - 1)])
+        .iter()
+        .enumerate()
+    {
+        let diff = sync_points[round - 1] - pulse_index;
+        cumulative_diff += diff;
+        let spacing = diff / virt_channel_count;
         // loop through virt channels
         for i in 0..virt_channel_count {
-            virt_channel_points.push((i,pulse_index + spacing*(i+1)))
+            let virt_channel_index = pulse_index + spacing * (i + 1);
+            virt_channel_samples.push((i, get_average_sample(&virt_channel_index,&full_data,&spacing)))
         }
     }
 
     // Find averages to
-    let average_diff = cumulitive_diff/(sync_points.len()-2);
-    let average_spacing = average_diff/virt_channel_count;
+    let average_diff = cumulative_diff / (sync_points.len() - 2);
+    let average_spacing = average_diff / virt_channel_count;
 
-    return virt_channel_points;
+    return virt_channel_samples;
+}
+
+/// Takes a fancy average of the sampled data
+fn get_average_sample(index: &usize,full_dataset: &Vec<f64>,width_of_channel: &usize) -> f64 {
+    let width_of_average: usize = width_of_channel/3;
+    let samples: Vec[f64] = full_dataset[index-width_of_average..(index+width_of_average+1)].to_vec().sort();
+
+    // Haven't done statistics properly yet, so here goes
+    let median = samples[samples.len()/2]; // Sensitive to noise, insensitive to skews
+    let mean = samples.iter().sum::<i32>() as f32 / numbers.len() as f32; // Sensitive to skews, insensitive to noise
+
+    // So we combine them, if the mean is too far off to not be skewed, then we use the median
+    if ((median-mean)/median).check_range(-0.1..0.1) {
+        return mean
+    } else {
+        return median
+    }
 }
